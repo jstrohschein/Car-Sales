d473a532f92cbeecf07806a5524dd30b6d6b3559
Jared Strohschein <45718292+jstrohschein@users.noreply.github.com>
2020-09-21 20:34:17 -0400

    Finished, but having issues with dispatch actions

---
+++ src/reducers/featuresReducer.js
@@ -0,0 +1,45 @@
+import { ADD_NEW_FEATURE, REMOVE_FEATURE } from '../actions/featuresActions';
+
+const initialState = {
+  additionalPrice: 0,
+  car: {
+    price: 26395,
+    name: '2019 Ford Mustang',
+    image:
+      'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+    features: []
+  },
+  additionalFeatures: [
+    { id: 1, name: 'V-6 engine', price: 1500 },
+    { id: 2, name: 'Racing detail package', price: 1500 },
+    { id: 3, name: 'Premium sound system', price: 500 },
+    { id: 4, name: 'Rear spoiler', price: 250 }
+  ]
+};
+
+
+export const featuresReducer = (state = initialState, action) => {
+
+  switch (action.type) {
+
+    case ADD_NEW_FEATURE:
+      const newFeature = {
+        name: action.payload
+      }
+      return {
+        ...state,
+        additionalFeatures: [...state.additionalFeatures, newFeature]
+      }
+    
+    case REMOVE_FEATURE:
+      return {
+        ...state,
+        additionalFeatures: state.additionalFeatures.filter(feature => feature.id !== action.payload)
+      }
+
+    default:
+      return state;
+
+  }
+
+}
